5bca6692c7882004447b5240e73ea8d601c2d524
Xavier Guertin <xavierguertin@gmail.com>
2022-12-03 16:53:36 -0500

    Modifications

--- A3/src/ElasticDatabase/Sequence.java
+++ A3/src/ElasticDatabase/Sequence.java
@@ -1,7 +1,131 @@
 package A3.src.ElasticDatabase;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class Sequence<E> extends ElasticERL<E> {
     ArrayList<DatabaseEntry<E>> sequence;
+
+    //constructor
+    public Sequence() {
+        this(0);
+    }
+    public Sequence(int n) {
+        super();
+        sequence = new ArrayList<DatabaseEntry<E>>(n);
+    }
+
+    //add an entry for the given key and value;
+    public void add(long key, E value) {
+        if(Long.toString(key).length() != 8) {
+            //System.out.println("Sequence Error: Length is not 8.\n");
+            return;
+        }
+
+        //clear the old key
+        try {
+            this.remove(key);
+        } catch(Exception e) {
+            sequence.add(new DatabaseEntry<E>(key, value));
+            size++;
+            return;
+        }
+        //System.out.println("Found duplicate for key " + key + ".\n");      
+        sequence.add(new DatabaseEntry<E>(key, value));
+        size++;
+    }
+
+    //remove the entry for the given key
+    public DatabaseEntry<E> remove(long key) {
+        try {
+            int index = search(key);
+            DatabaseEntry<E> temp = sequence.get(index);
+            sequence.remove(index);
+            size--;
+            return temp;
+
+        } catch (NullPointerException e) {
+            System.out.println("Key " + key + " does not exist");
+            return null;
+        }
+    }
+
+    public E getValues(long key) {
+        try {
+            int index = search(key);
+            return sequence.get(index).getValue();
+
+        } catch (NoSuchElementException e) {
+            System.out.println("Key " + key + " does not exist");
+        }
+
+        return null;
+    }
+
+    public long nextKey(long key) {
+        try {
+            int index = search(key);
+            if(sequence.get(index+1) != null) {
+                return sequence.get(index+1).getKey();
+            } else {
+                throw new NoSuchElementException();
+            }
+
+        } catch (NoSuchElementException e) {
+            System.out.println("Next key of " + key + " does not exist.");
+        }
+        return 0;
+    }
+
+    public long prevKey(long key) {
+        try {
+            int index = search(key);
+            if(sequence.get(index-1) != null) {
+                return sequence.get(index-1).getKey();
+            } else {
+                throw new NoSuchElementException();
+            }
+
+        } catch (NoSuchElementException e) {
+            System.out.println("Prev key of " + key + " does not exist.");
+        }
+        return 0;
+    }
+
+    public int search(long key) throws NoSuchElementException {
+        for (int i = 0; i<size; ++i) {
+            if(sequence.get(i).getKey() == key) {
+                return i;
+            }
+        }
+        throw new NoSuchElementException();
+    }
+
+    //return all keys in ElastricERL as a sorted sequence;
+    public Long[] allKeys() {
+        Long[] result = new Long[super.getSize()];
+        int ctr = 0;
+        for(DatabaseEntry<E> each : sequence) {
+            result[ctr] = each.getKey();
+            ctr++;
+        }
+        insertionSort(result);
+        return result;
+    }
+
+    //sorting the sequence using insertion Sort
+    public void insertionSort(Long[] keys) {
+        int n = keys.length;
+        for(int i = 1; i< n; ++i) {
+            long current = keys[i];
+            int j = i-1;
+
+            while(j >= 0 && current < keys[j]) {
+                keys[j+1] = keys[j];
+                j--;
+
+            }
+            keys[j+1] = current;
+        }
+    }
 }

18b03a720ee0b832a2ff7808bb53f9214e5154dd
Bayazeed Rahman <99050468+blazefire16@users.noreply.github.com>
2022-12-03 13:16:18 -0500

    Add files via upload

---
+++ A3/src/ElasticDatabase/DatabaseEntry.java
@@ -0,0 +1,38 @@
+package ElasticDatabase;
+
+//This class defines an entry object that holds a integer key and a String value 
+public class DatabaseEntry 
+{
+	private int key;
+	private String value; 
+	
+	//Constructor
+	public DatabaseEntry(int key, String value)
+	{
+		this.key = key;
+		this.value = value; 
+	}
+	
+	public int getKey()
+	{
+		return key; 
+	}
+	
+	public String getValue() 
+	{
+		return value; 
+	}
+	
+	protected void setKey(int key)
+	{
+		this.key = key;
+	}
+	
+	protected void setValue(String value)
+	{
+		this.value = value;
+	}
+	
+	
+
+}
---
+++ A3/src/ElasticDatabase/ElasticERL.java
@@ -0,0 +1,84 @@
+package ElasticDatabase;
+import java.util.Random; 
+import ElasticDatabase.DatabaseEntry;
+
+
+public class ElasticERL 
+{
+	//We have an integer for the threshold and one for an array of entries. 
+	private int EINThreshold;
+	private DatabaseEntry smallEntries[]; 
+	
+	//Default Constructor (ignore for now)
+	public ElasticERL()
+	{
+		this.EINThreshold = 100;
+	}
+	
+	//Constructor which allows user to choose size
+	public ElasticERL(int Size)
+	{
+		//Check for valid size
+		if(Size <= 100 || Size >= 500000)
+		{
+			System.out.println("Unable to set the threshold, size is not within the range 100 to 500000"); 
+			System.exit(0);1
+		}
+		//In case of valid size, initialize the variable to that size
+		else
+		{
+		this.EINThreshold = Size;
+		}
+		
+		//If we have a small number of entries
+		if(Size < 1000)
+		{
+			//Initialize an array of DatabaseEntry to the size
+			smallEntries = new DatabaseEntry[Size];
+		}
+		
+		//if we have a larger amount of entries
+		if(Size > 1000)
+		{
+			
+		}
+	}
+
+
+	public int getEINThreshold() 
+	{
+		return this.EINThreshold;
+	}
+	
+	
+	public void setEINThreshold(int Size) 
+	{
+		if(Size <= 100 || Size >= 500000)
+		{
+			System.out.println("Unable to set the threshold, size is not within the range 100 to 500000"); 
+			System.exit(0);
+		}
+		else
+		{
+		this.EINThreshold = Size;
+		}
+	} 
+	
+	//A method to generate a random 8-digit number
+	public int generate() 
+	{
+		int myRandomNumber = (int) Math.pow(10, 8 - 1);
+      	return myRandomNumber + new Random().nextInt(9 * myRandomNumber);
+	}
+	
+	
+	
+	//main for testing (TEMPORARY)
+	public static void main(String[] args) 
+	{
+		ElasticERL x = new ElasticERL(); 
+		System.out.println(x.generate()); 
+	}
+	
+
+}
